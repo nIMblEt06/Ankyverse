9addb73896f8e3f8108e197be04971dfeeba96a5
Shwetabh Niket <f20210923@pilani.bits-pilani.ac.in>
2023-06-19 22:02:39 +0530

    feat: node script to get final data in folders

---
+++ getNFTs.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config()
+
+async function fetchData() {
+    try {
+        const response = await axios.get(process.env.GET_CHARACTERS_API);
+        const jsonData = response.data;
+        const dataArray = jsonData.fetalCharacters;
+        dataArray.slice(0, 20).forEach(async (data, index) => {
+            const filename = `${index}`;
+            await writeDataToFile(data, filename);
+            const imagename = `${index}`;
+            await downloadImage(data.upscaledImageUrls[0], imagename);
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+    }
+}
+
+fetchData();
+
+async function writeDataToFile(data, filename) {
+    const filePath = `metadata/${filename}.json`;
+
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    console.log(`Data written to file: ${filePath}`);
+}
+
+async function downloadImage(url, filename) {
+    try {
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
+        const imageData = response.data;
+        const imagePath = `images/${filename}.png`;
+        fs.writeFileSync(imagePath, Buffer.from(imageData, 'binary'));
+        console.log(`Image downloaded: ${filename}`);
+    } catch (error) {
+        console.error('Error downloading image:', error.message);
+    }
+}
